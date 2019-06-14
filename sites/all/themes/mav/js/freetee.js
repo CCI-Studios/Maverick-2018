c85ff70405aaabef023892a0e48749b658216b74
Chris Morris <cmorris@ccistudios.com>
2019-06-14 13:16:58 -0400

    finished changes to product detail page for the free tshirt

--- sites/all/themes/mav/js/freetee.js
+++ sites/all/themes/mav/js/freetee.js
@@ -2,10 +2,14 @@
   $(function () {
     if (window.location.pathname === '/product/be-maverick-fitted-tee') {
       $('.node-product-display .field-name-commerce-price .field-item').text('FREE - LIMIT 1 PER ORDER.');
-      $('.node-product-display .commerce-add-to-cart').find('.form-item-quantity, .form-submit').hide();
-      $('.node-product-display').on('click', '.commerce-add-to-cart .form-item-attributes-field-shirt-size label', function () {
-        var size = $('.node-product-display .commerce-add-to-cart [name="attributes[field_attribute_color]"]:checked').parent().find('label').text();
-        console.log(size);
+      $('.node-product-display').addClass('hide-add-to-cart');
+      $('.node-product-display').on('click', '.commerce-add-to-cart .form-type-radio.form-item-attributes-field-attribute-color', function (e) {
+        var colour = $(e.target).closest('.form-type-radio.form-item-attributes-field-attribute-color').find('label.option').text().trim();
+        $('.block--free-tshirt-shipping .webform-component--colour input').val(colour);
+      });
+      $('.node-product-display').on('click', '.commerce-add-to-cart .form-item-attributes-field-shirt-size label', function (e) {
+        var size = $(e.target).text().trim();
+        $('.block--free-tshirt-shipping .webform-component--size input').val(size);
       });
     }
   });
