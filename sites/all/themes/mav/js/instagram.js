7784d3046ebd2b98d1b567f2d4d87a7a3ac85255
Chris Morris <cmorris@ccistudios.com>
2019-07-02 09:28:10 -0400

    use pixlee instagram feed

--- sites/all/themes/mav/js/instagram.js
+++ sites/all/themes/mav/js/instagram.js
@@ -1,25 +1,7 @@
 (function ($) {
 	$(function () {
     if ($(".instagram-container").length) {
-      $.ajax({
-        "type": "GET",
-        "url": "https://api.instagram.com/v1/users/self/media/recent?count=4&access_token=3152797019.1677ed0.9b393771eecd4944bd897be69db77dda",
-        "success": success,
-        "dataType": "jsonp"
-      });
+      $(".instagram-container").html("<a href='https://www.instagram.com/be.a.maverick/' target='_blank'></a><div id='pixlee_container'></div><script type='text/javascript'>window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'V-PopfRaTmhtfoC_9gtA'});Pixlee.addSimpleWidget({widgetId:'19287'});};</script><script src='//instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js'></script>");
     }
 	});
-	function success (json) {
-		if (json && json.data) {
-      var html = "";
-			for (var i = 0; i < 4; i++) {
-				if (json.data[i] && json.data[i].images && json.data[i].images.low_resolution && json.data[i].images.low_resolution.url) {
-          var image = json.data[i].images.low_resolution.url;
-          var link = json.data[i].link;
-					html += "<div class=\"views-row\"><a href=\"" + link + "\" target=\"_blank\" style=\"background-image:url('" + image + "');\"></div>";
-				}
-      }
-      $(".instagram-container").html(html);
-		}
-	}
 }(jQuery));
